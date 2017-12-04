c11d8e49597388daee274adf2522382c03ad088b
xingshanghe <xingshanghe@gmail.com>
2017-12-04 10:07:00 +0800

    部分小修改

--- pages/computed-network/vpc/list.vue
+++ pages/computed-network/vpc/list.vue
@@ -1,11 +1,25 @@
 <template>
-  <div>{{$route.path}}</div>
+  <section>
+    <el-row>
+      <el-col :span="16">
+        <listTitlePills  />
+      </el-col>
+      <el-col class="top-buttons" :span="8">
+        <el-button size="small"><i class="el-icon-refresh "></i>刷新</el-button>
+        <el-button type="primary"  size="small"><i class="el-icon-plus "></i>新增</el-button>
+      </el-col>
+    </el-row>
+  </section>
 </template>
 <script>
+import listTitlePills from '~/components/console/list-title-pills.vue';
 export default {
   head: {
-    title: '实例列表'
+    title: '专有网络'
   },
-  middleware: 'authenticated'
+  middleware: 'authenticated',
+  components: {
+    listTitlePills
+  }
 };
 </script>
