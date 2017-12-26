5476749bfb3b1ac91e7c09028ba5c6352b3e3a1c
xingshanghe <xingshanghe@gmail.com>
2017-12-26 09:29:57 +0800

    升级部分库

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,36 @@
 <template>
   <div>
-    <el-button type="primary" plain size="small" @click="handleClick">点击</el-button>
-    <v-chart :force-fit="true" :height="height" :data="data" :data-pre="dataPre" :data-mapping="dataMapping" :scale="scale">
+    <v-chart :force-fit="true" :height="height" :data="data" :scale="scale">
       <v-tooltip />
       <v-axis />
-      <v-stack-bar :v-style="stackBarStyle" />
+      <v-line :position="'year*value'" />
+      <v-point :position="'year*value'" />
     </v-chart>
     <!-- <v-lite-chart :stack-bar="true" :height="400" :data="data" :data-pre="dataPre" :dataMapping="dataMapping" :forceFit="true" /> -->
   </div>
 </template>
 
 <script>
-import { data, dataMapping, dataPre, scale } from '~/static/data.js';
+const data = [
+  { year: '1991', value: 3 },
+  { year: '1992', value: 4 },
+  { year: '1993', value: 3.5 },
+  { year: '1994', value: 5 },
+  { year: '1995', value: 4.9 },
+  { year: '1996', value: 6 },
+  { year: '1997', value: 7 },
+  { year: '1998', value: 9 },
+  { year: '1999', value: 13 }
+];
+
+const scale = [{
+  dataKey: 'value',
+  min: 0
+}, {
+  dataKey: 'year',
+  min: 0,
+  max: 1
+}];
 export default {
   head: {
     title: '管理控制台'
@@ -22,21 +41,11 @@ export default {
   data() {
     return {
       data,
-      dataMapping,
-      dataPre,
       scale,
-      height: 400,
-      stackBarStyle: {
-        stroke: '#fff',
-        lineWidth: 1
-      }
+      height: 400
     };
   },
   methods: {
-    handleClick: function() {
-      this.height = 600;
-      this.stackBarStyle.lineWidth = 10;
-    }
   }
 };
 </script>
